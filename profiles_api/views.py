9d5b62807d854f328da593115505b2738ce9861b
benjamin2300 <benjamin2300.b98902064@gmail.com>
2019-12-08 21:51:22 +0800

    Add profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,15 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 # Create your views here.
 
 class HelloApiView(APIView):
   """Test API View"""
   
   serializer_class = serializers.HelloSerializer
-
 
   def get(self, request, format=None):
     """Return a list of APIView features"""
@@ -82,3 +85,9 @@ class HelloViewSet(viewsets.ViewSet):
   
   def destroy(self, request, pk=None):
     return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+  serializer_class = serializers.UserProfileSerializer
+  queryset = models.UserProfile.objects.all()
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (permissions.UpdateOwnProfile,)
